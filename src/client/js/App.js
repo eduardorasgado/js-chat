ac5c0f38bc738a9768a201681e50940d52e64823
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2023-08-16 22:06:03 -0600

    App component refactored p1

--- src/client/js/App.js
+++ src/client/js/App.js
@@ -1,49 +1,80 @@
 import React, { useEffect } from 'react';
-import { Provider } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   HashRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from 'react-router-dom';
 import HomeView from './views/Home'
 import WelcomeView from './views/Welcome';
 import SettingsView from './views/Settings';
 import ChatView from './views/Chat';
 import Navbar from "./components/Navbar";
-import configureStore from './store';
 import { listenToAuthChanges } from './actions/auth';
+import StoreProvider from './store/StoreProvider';
+import LoadingView from './components/shared/LoadingView';
 
-const store = configureStore();
-
-function App() {
-
-  useEffect(() => {
-    store.dispatch(listenToAuthChanges());
-  }, [store.dispatch]);
+function AuthRoute({children, ...rest}) {
+  const user = useSelector(({auth}) => auth.user);
 
   return (
-    <Provider store={store}>
-      <Router>
-        <Navbar />
-        <div className='content-wrapper'>
-          <Switch>
-            <Route path="/" exact>
-              <WelcomeView />
-            </Route>
-            <Route path="/home"t>
-              <HomeView />
-            </Route>
-            <Route path="/chat/:id">
-              <ChatView />
-            </Route>
-            <Route path="/settings">
-              <SettingsView />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </Provider>
+    <Route render={() => {
+      user ? children : <Redirect to='/'/>;
+    }} />
+  );
+}
+
+const ContentWrapper = ({children}) => {
+  return (
+    <div className='content-wrapper'>
+      {children}
+    </div>
+  )
+}
+
+function ChatApp() {
+  const dispatch = useDispatch();
+
+  const isChecking = useSelector(({auth}) => auth.isChecking);
+
+  useEffect(() => {
+    dispatch(listenToAuthChanges());
+  }, [dispatch]);
+
+  if(isChecking) {
+    return <LoadingView message={'Loading...'}/>
+  }
+
+  return (    
+    <Router>
+      <Navbar />
+      <ContentWrapper>
+        <Switch>
+          <Route path="/" exact>
+            <WelcomeView />
+          </Route>
+          <Route path="/home">
+            <HomeView />
+          </Route>
+          <Route path="/chat/:id">
+            <ChatView />
+          </Route>
+          <Route path="/settings">
+            <SettingsView />
+          </Route>
+        </Switch>
+      </ContentWrapper>
+    </Router>
   );
 };
+
+function App() {
+  return (
+    <StoreProvider>
+      <ChatApp />
+    </StoreProvider>
+  );
+}
 
 export default App;
